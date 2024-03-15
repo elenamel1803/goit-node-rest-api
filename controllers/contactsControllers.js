4eca59c12873e074764c1749c5336e23c07d75a5
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-03-15 23:31:02 +0200

    add controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,80 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  refreshContact,
+  removeContact,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (_, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(404, error.message);
+    }
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await refreshContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+    // res.json({
+    //   message: "Delete success"
+    // });
+  } catch (error) {
+    next(error);
+  }
+};
