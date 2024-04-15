8654fd22f1801039f63a561176d70c5e9687d585
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-04-16 02:52:44 +0300

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,17 +1,35 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "string.base": "Name must be a string",
+    "any.required": "Missing required field name",
+  }),
+  email: Joi.string().required().messages({
+    "string.base": "Email must be a string",
+    "any.required": "Missing required field email",
+  }),
+  phone: Joi.string().required().messages({
+    "string.base": "Phone must be a string",
+    "any.required": "Missing required field phone",
+  }),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
+  name: Joi.string().messages({
+    "string.base": "Name must be a string",
+  }),
+  email: Joi.string().messages({
+    "string.base": "Email must be a string",
+  }),
+  phone: Joi.string().messages({
+    "string.base": "Phone must be a string",
+  }),
 });
 
 export const updateStatusContactSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean().required().messages({
+    "boolean.base": "Favorite must be true or false",
+    "any.required": "Missing required field favorite",
+  }),
 });
